cdbb468a018f5644e3253bb83d5fb26b4595df55
Dapplink <67794448+dapplink@users.noreply.github.com>
2021-02-17 13:53:30 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event)
+    event.waitUntil(
+	caches.open('static')
+	    .then(function(cache) {
+		console.log('[Service Worker] Precaching App Shell')
+		cache.addAll([
+		    '/',
+		    '/index.html',
+		    '/abi.js',
+		    '/app.js',
+		    'ethereumjs-tx.js'
+		])
+	    })
+    )
+})
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ....', event)
+    return self.clients.claim()
+})
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+	caches.match(event.request)
+	    .then(function(response) {
+		if (response) {
+		    return response
+		} else {
+		    return fetch(event.request)
+		}
+	    })
+    )
+})
